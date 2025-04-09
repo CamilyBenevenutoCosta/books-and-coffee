e9a10d995a26a8860caa41c72031c51dd18c5468
Cami_Costa <camily.bc.veleiros@gmail.com>
2025-04-09 16:02:19 -0300

    efeito digitação

--- js/script.js
+++ js/script.js
@@ -37,6 +37,20 @@
         select('body').classList.toggle('mobile-nav-active')
         this.classList.toggle('bi-list')
         this.classList.toggle('bi-x')
-      }) 
+      })
+      
+      /* Efeito Digitação */
+
+      const typed = select('.typed');
+
+      if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+
+        new Typed('.typed', {
+          strings: typed_strings,  
+        })
+
+    }
 
 })()
